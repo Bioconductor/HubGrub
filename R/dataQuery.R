d2e3df3bfe1111a652e4e1e8efe3a08d14820b83
Kayla-Morrell <kayla.morrell@roswellpark.org>
2021-11-02 12:35:50 -0400

    Start of import functionality

---
+++ R/dataQuery.R
@@ -0,0 +1,52 @@
+#' importFrom AnnotationDbi dbFileConnect dbfile
+#' importFrom DBI dbGetQuery dbDisconnect
+#' @noRd
+.resourceLocation <- function (x) {
+    query <-
+        'SELECT DISTINCT resources.ah_id, location_prefixes.location_prefix
+        FROM resources, location_prefixes
+        WHERE location_prefixes.id == resources.location_prefix_id'
+
+    con <- dbFileConnect(dbfile(x))
+    mat <- dbGetQuery(con, query)
+    dbDisconnect(con)
+    mat
+}
+
+.idsInfo <- function (hub, ids) {
+    dat <- .resourceLocation(hub)
+    sub_dat <- match(ids, dat$ah_id)
+
+    res <- dat[sub_dat,]
+    res
+}
+
+#' Import data of hub object
+#' 
+#' @param hub A hub object, either AnnotationHub or ExperimentHub
+#' @param ids The hub id(s) of interest.
+#' @param which A range data structure, like a 'GRanges'.
+#' 
+#' importFrom rtracklayer import.bw import.chain import.2bit import.bed
+#' importFrom tools file_ext
+#' 
+#' @return A 'GRanges'.
+#' 
+#' @examples
+#' ah = AnnotationHub()
+#' which <- GRanges(c("chr2", "chr2"), IRanges(c(1, 300), c(400, 1000)))
+#' importData(ah, "AH49544", which)
+importData <- function (hub, ids, which) {
+    tbl <- .idsInfo(hub, ids)
+
+    datPath <- mcols(hub[ids])$rdatapath
+
+    path <- paste(tbl$location_prefix, datPath, sep = "")    
+    ext <- file_ext(path)
+
+    if (ext == "bigwig" || ext == "bw") {
+        import.bw(path, which)
+    } else {
+        message("could not be imported")
+    }
+}
